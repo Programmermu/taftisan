cd9e39d5ac12f8278edae532c02f11a93fbeb163
Mustain <hadapilahdengansenyuman@gmail.com>
2024-03-25 04:10:49 +0700

    "tambah fitur enter pada loncat bait"

--- baitalfiyah.php
+++ baitalfiyah.php
@@ -2,7 +2,7 @@
 include("koneksi.php");
 
 // Query untuk mengambil data dari tabel
-$sql = "SELECT * from bait";
+$sql = "SELECT * from bait where nama_nadhom ='ALFIYYAH' order by  no_bait asc";
 $result = mysqli_query($conn, $sql);
 
 // Periksa apakah query berhasil dieksekusi
@@ -33,8 +33,19 @@ mysqli_close($conn);
     display: inline-block;
     margin: 0;
     border-bottom: 2px dashed gainsboro;
-    text-align: center;
-    width: 85%;
+    text-align: right;
+    width: 88%;
+    transition: opacity 2s ease-in-out;
+  }
+
+  .genap {
+    border-bottom-style: ridge;
+
+  }
+
+  .satar_tsani,
+  .satar_awal {
+    transition: opacity 0.5s ease-in-out;
   }
 
   a {
@@ -57,6 +68,27 @@ mysqli_close($conn);
     background-color: aliceblue;
   }
 
+  #footer {
+    position: sticky;
+    bottom: 0;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    z-index: 100;
+    width: 100%;
+    background-color: azure;
+  }
+
+  button {
+    padding: 3px;
+    background-color: aquamarine;
+    border-radius: 5px;
+  }
+
+  #toggle {
+    padding: 10px;
+  }
+
   #backheader {
     width: auto;
   }
@@ -67,28 +99,33 @@ mysqli_close($conn);
     <label>Loncat :
     </label>
     <input type="number" name="nobait" id="nobait">
-    <button onclick="focusElement()">Loncat</button>
+    <button onclick="focusElement()" id="loncat">Loncat</button>
     <a href="index.php" id="backheader">Kembali ke Awal</a>
   </div>
   <div class="data">
     <?php foreach ($data as $bait) { ?>
       <p>
-      <div class="satar">
+      <div class="satar <?php if ($bait['no_bait'] % 2 == 0) {
+                          echo "genap";
+                        } ?>">
         <span class="satar_awal">
           <?= $bait['satar_awal']; ?>
-        </span> #
+        </span>
         <span class="satar_tsani">
-          <?= $bait['satar_tsani']; ?>
+          # <?= $bait['satar_tsani']; ?>
         </span>
       </div>
       <span id="<?= $bait['no_bait']; ?>" tabindex="0"> :
         <?= $bait['no_bait']; ?>
       </span>
-      </p>
       <?php if ($bait['no_bait'] % 100 == 0) { ?>
         <a href="index.php" id="backToStart">Kembali ke Awal</a>
       <?php } ?>
+      </p>
     <?php } ?>
+  </div>
+  <div id="footer">
+    <button onclick="toggleMode()" id="toggle">Ubah Mode</button>
   </div>
 
 </body>
@@ -99,14 +136,36 @@ mysqli_close($conn);
   let satar_tsani = document.getElementsByClassName("satar_tsani");
 
   function toggleMode() {
+    for (let i = 0; i < satar_awal.length; i++) {
+      if (mode) {
+        satar_awal[i].textContent = "..." + bait[i].satar_awal.split(' ').slice(0, 3).join(' ');
+        satar_tsani[i].hidden = true;
+      } else {
+        satar_awal[i].textContent = bait[i].satar_awal;
+        satar_tsani[i].style.opacity = 0;
+        satar_tsani[i].hidden = false;
+        setTimeout(function() {
+          satar_tsani[i].style.opacity = 1;
+        }, 200);
+      }
+    }
     mode = !mode;
   }
 
   function focusElement() {
     var nomor = document.getElementById("nobait");
-    var inputElement = document.getElementById(nomor.value);
-    inputElement.focus();
+    if (nomor.value > 0) {
+      var inputElement = document.getElementById(nomor.value);
+      inputElement.focus();
+    } else {
+      alert("masukkan angka terlebih dahulu")
+    }
   }
+  document.getElementById('nobait').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      focusElement();
+    }
+  });
 </script>
 
 </html>
