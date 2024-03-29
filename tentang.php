ce17e760be39cb39e459bfd427449397d0c78edb
Mustain <hadapilahdengansenyuman@gmail.com>
2024-03-29 17:19:52 +0700

    tambah style tentang

--- tentang.php
+++ tentang.php
@@ -4,47 +4,56 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Tentang Web Taftisan</title>
   <style>
-    /* Gaya umum untuk teks */
     body {
       font-family: Arial, sans-serif;
       line-height: 1.6;
       color: #333;
     }
 
-    /* Gaya untuk judul level 1 */
     h1 {
       font-size: 2em;
       font-weight: bold;
       margin-bottom: 0.5em;
     }
 
-    /* Gaya untuk judul level 2 */
     h2 {
       font-size: 1.5em;
       font-weight: bold;
       margin-bottom: 0.5em;
     }
 
-    /* Gaya untuk judul level 3 */
     h3 {
       font-size: 1.2em;
       font-weight: bold;
       margin-bottom: 0.5em;
     }
 
-    /* Gaya untuk judul level 4 */
     h4 {
       font-size: 1em;
       font-weight: bold;
       margin-bottom: 0.5em;
     }
 
-    /* Gaya untuk paragraf */
     p {
       font-size: 1em;
       margin-bottom: 1em;
+    }
+
+    ul {
+      list-style-type: disc;
+      margin-left: 10px;
+    }
+
+    ol {
+      list-style-type: decimal;
+      margin-left: 10;
+    }
+
+    li {
+      font-weight: 400;
+      margin-bottom: 5px;
     }
   </style>
 </head>
@@ -52,26 +61,52 @@
 <body>
   <h1>Tentang</h1>
   <h2>Website Taftisan</h2>
-  <h3>Tujuan</h3>
-  <p>Website ini di buat sesederhana mungkin dengan tujuan memudahkan dan menjaga niat dari para santri yang ingin menghafal Alfiyyah Ibn Malik, dengan beberapa fitur yang membuat mereka bisa aktif dan test hafalannya sendiri</h3>
-  <h3>Fitur</h3>
-  <h4>Bait Alfiyah Lengkap :</h4>
-  <p>Saat ini untuk bait yang di update baru 500 bait, dengan fitur loncat bait, dan mode awal satar untuk melatih hafalan santri</p>
-  <h4>Mode Quiz</h4>
-  <p>Mode untuk melatih melanjutkan bait dengan bait acak, cocok untuk simulasi taftisan</p>
-  <h4>Mode Lalaran</h4>
-  <p>Mode untuk melatih melanjutkan bait dengan bait berutan sesuai setting, cocok untuk hafalan baru atau yang belum kuat hafalannya</p>
-  <h4>Mode Mumarrin</h4>
-  <p>Mode untuk melatih melanjutkan bait untuk mengetes orang lain, cocok untuk gambaran taftisan dengan bantuan teman</p>
-  <h4>Riwayat</h4>
-  <p>Hasil Nilai sesuai dengan nama yang di input terakhir kali</p>
-  <h4>Logout</h4>
-  <p>Menghapus nama dan data di setting dari database (untuk yang masuk di nilai akhir maka tetap tersimpan)</p>
-  <h3>Update</h3>
-  <p>Webbsite ini akan di update hanya jika pemilik memiliki waktu luang, oleh karena itu mohon doa, dan bagi memiliki saran atau kritik serta mereka yang ingin ikut mengembangan bisa menghubungi pemilik di IG @hadapilahdengansenyuman</p>
-  <p>Saya mengakui banyak kekurangan baik design, atau fitur yang masih memiliki bug (error), untuk koreksi bisa menghubungi kontak di atas</p>
-  <h3>Penutup</h3>
-  <p>Janganlah menunda untuk memulai sesuatu, walau itu burik sekalipun (#Jhon IQ7)</p>
+  <ol>
+    <li>
+      <h3>Tujuan</h3>
+      <p>Website ini di buat sesederhana mungkin dengan tujuan memudahkan dan menjaga niat dari para santri yang ingin menghafal Alfiyyah Ibn Malik, dengan beberapa fitur yang membuat mereka bisa aktif dan test hafalannya sendiri</h3>
+    </li>
+    <li>
+      <h3>Fitur</h3>
+
+      <ul>
+
+        <li>
+          <h4>Bait Alfiyah Lengkap :</h4>
+          <p>Saat ini untuk bait yang di update baru 500 bait, dengan fitur loncat bait, dan mode awal satar untuk melatih hafalan santri</p>
+        </li>
+        <li>
+          <h4>Mode Quiz</h4>
+          <p>Mode untuk melatih melanjutkan bait dengan bait acak, cocok untuk simulasi taftisan</p>
+        </li>
+        <li>
+          <h4>Mode Lalaran</h4>
+          <p>Mode untuk melatih melanjutkan bait dengan bait berutan sesuai setting, cocok untuk hafalan baru atau yang belum kuat hafalannya</p>
+        </li>
+        <li>
+          <h4>Mode Mumarrin</h4>
+          <p>Mode untuk melatih melanjutkan bait untuk mengetes orang lain, cocok untuk gambaran taftisan dengan bantuan teman</p>
+        </li>
+        <li>
+          <h4>Riwayat</h4>
+          <p>Hasil Nilai sesuai dengan nama yang di input terakhir kali</p>
+        </li>
+        <li>
+          <h4>Logout</h4>
+          <p>Menghapus nama dan data di setting dari database (untuk yang masuk di nilai akhir maka tetap tersimpan)</p>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <h3>Update</h3>
+      <p>Webbsite ini akan di update hanya jika pemilik memiliki waktu luang, oleh karena itu mohon doa, dan bagi memiliki saran atau kritik serta mereka yang ingin ikut mengembangan bisa menghubungi pemilik di IG @hadapilahdengansenyuman</p>
+      <p>Saya mengakui banyak kekurangan baik design, atau fitur yang masih memiliki bug (error), untuk koreksi bisa menghubungi kontak di atas</p>
+    </li>
+    <li>
+      <h3>Penutup</h3>
+      <p>Janganlah menunda untuk memulai sesuatu, walau itu burik sekalipun (#Jhon IQ7)</p>
+    </li>
+  </ol>
 </body>
 
 </html>
